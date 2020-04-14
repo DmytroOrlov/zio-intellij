fff6f1fb9bb409ad85d8fefbfdee6ea442b342b6
Dmytro Orlov <orlov.dm@gmail.com>
2020-04-14 21:22:50 +0200

    revert plain trait compatibility

--- src/main/scala/zio/intellij/synthetic/macros/ModulePatternAccessible.scala
+++ src/main/scala/zio/intellij/synthetic/macros/ModulePatternAccessible.scala
@@ -8,23 +8,24 @@ import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef.SyntheticMembe
 import org.jetbrains.plugins.scala.lang.psi.types.PhysicalMethodSignature
 
 class ModulePatternAccessible extends SyntheticMembersInjector {
-  private val accessible17 = "zio.macros.annotation.accessible"
-  private val accessible = "zio.macros.accessible"
+  private val accessible = Seq(
+    "zio.macros.annotation.accessible",
+    "zio.macros.accessible",
+  )
 
   private def hasAccessible(source: ScTypeDefinition) =
-    source.findAnnotationNoAliases(accessible17) != null
+    accessible.exists(a => source.findAnnotationNoAliases(a) != null)
 
   private def annotationFirstParam(scAnnotation: ScAnnotation): Option[String] =
     scAnnotation.annotationExpr.getAnnotationParameters.collectFirst {
       case sl: ScLiteralImpl => sl.getValue()
     }
 
-  private def helperObjectExtension(annotation: ScAnnotation, sco: ScObject, structuralTyping: Boolean): Seq[String] =
-    annotationFirstParam(annotation)
-      .map(name =>
-        if (structuralTyping) s"def $name : ${sco.qualifiedName} = ???"
-        else s"def $name : ${sco.qualifiedName}.Service[${sco.qualifiedName}] = ???")
-      .toSeq
+  private def helperObjectExtension(name: Option[String], sco: ScObject, plain: Boolean): Seq[String] =
+    name.map { n =>
+      if (plain) s"def $n : ${sco.qualifiedName} = ???"
+      else s"def $n : ${sco.qualifiedName}.Service[${sco.qualifiedName}] = ???"
+    }.toSeq
 
   private def accessorTraitExtension(sco: ScObject): String = {
     val serviceTrait = sco.typeDefinitions.find(_.name == "Service")
@@ -45,9 +46,9 @@ class ModulePatternAccessible extends SyntheticMembersInjector {
     signatures
   }
 
-  private def findAccessibleMacroAnnotation(accessible: String, sco: ScObject): Option[ScAnnotation] = {
+  private def findAccessibleMacroAnnotation(accessible: Seq[String], sco: ScObject): Option[ScAnnotation] = {
     val companion = sco.fakeCompanionClassOrCompanionClass
-    Option(companion.getAnnotation(accessible)).collect {
+    accessible.map(companion.getAnnotation).collectFirst {
       case a: ScAnnotation => a
     }
   }
@@ -55,11 +56,12 @@ class ModulePatternAccessible extends SyntheticMembersInjector {
   override def injectMembers(source: ScTypeDefinition): Seq[String] =
     source match {
       case sco: ScObject =>
-        val annotation17 = findAccessibleMacroAnnotation(accessible17, sco)
+        val plain = !sco.typeDefinitions.exists(_.name == "Service")
+        val annotation17 = findAccessibleMacroAnnotation(accessible.head :: Nil, sco)
         annotation17.fold {
-          val annotation = findAccessibleMacroAnnotation(accessible, sco)
-          annotation.map(_ => accessorMethodsExtension(sco)).getOrElse(Nil)
-        }(a => helperObjectExtension(a, sco, !sco.typeDefinitions.exists(_.name == "Service")) :+ accessorTraitExtension(sco))
+          if (plain) helperObjectExtension(Some(">"), sco, plain)
+          else accessorMethodsExtension(sco)
+        }(a => helperObjectExtension(annotationFirstParam(a), sco, plain) :+ accessorTraitExtension(sco))
       case _ =>
         Nil
     }
